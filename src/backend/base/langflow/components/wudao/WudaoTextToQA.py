ad3f4a7c676a45c74523de9898cf3580d5ec478a
andysong <sczhoustill@126.com>
2024-09-06 14:14:51 +0800

    1、新组件：WudaoTextToQA。将List[str],转化为QA。

--- src/backend/base/langflow/components/wudao/WudaoTextToQA.py
+++ src/backend/base/langflow/components/wudao/WudaoTextToQA.py
@@ -6,6 +6,7 @@ from langchain_core.prompts import PromptTemplate
 from langflow.base.wudao_constants import PROMPT_TEXT_TO_QA
 from langflow.custom import Component
 from langflow.io import DataInput, HandleInput, Output
+from langflow.schema import Data
 
 
 class WudaoTextToQAComponent(Component):
@@ -23,16 +24,23 @@ class WudaoTextToQAComponent(Component):
         Output(display_name="Chunks", name="chunks", method="parse_data"),
     ]
 
-    def parse_data(self) -> List[str]:
+    def parse_data(self) -> List[Data]:
         data = self.data if isinstance(self.data, list) else [self.data]
         chunks = []
         count = 1
         all_count = len(data)
         for text in data:
             sequence = PromptTemplate(template=PROMPT_TEXT_TO_QA, input_variables=["text"]) | self.llm
-            result = sequence.invoke(text).content
+            content = sequence.invoke(text).content
+            if content[0:7] == "```json":
+                content = content[7:-3]
+                print(content)
+                results = json.loads(content)
+                if isinstance(results, list):
+                    for result in results:
+                        chunks.append(Data(data=result))
             print(str(count) + "/" + str(all_count))
             count += 1
-            chunks.extend(json.loads(result))
+
         self.status = chunks
         return chunks
